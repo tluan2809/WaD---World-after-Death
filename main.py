2191a796b84741e18344580e9772bd28393e819e
beginner2809 <ngulogaming@gmail.com>
2021-06-21 18:04:46 +0700

    a

--- main.py
+++ main.py
@@ -1,2 +1,23 @@
 import pygame
 
+pygame.init()
+# display screen
+screen = pygame.display.set_mode((1200, 800))
+pygame.display.set_caption('World after Death')
+screen.fill('white')
+
+# create function for skills
+def Attack():
+    pass
+
+def Heal():
+    pass
+
+# generate skills, player status
+skill = [Attack, Heal]
+player_input_skill = ''
+player_class = ''
+heath = 0
+energy = 0
+
+pygame.display.update()
